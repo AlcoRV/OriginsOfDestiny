09038ea67a422d612c5e0ad3fe3127dfa22abb78
Коршунов <alexander.korshunov@advalange.com>
2024-02-29 17:15:37 +0300

    updated main hero model

--- OriginsOfDestiny.Game/Models/Entity/MainHero.cs
+++ OriginsOfDestiny.Game/Models/Entity/MainHero.cs
@@ -1,9 +1,13 @@
-﻿namespace OriginsOfDestiny.Game.Models.Entity;
+﻿using OriginsOfDestiny.Game.Enums;
+
+namespace OriginsOfDestiny.Game.Models.Entity;
 
 public class MainHero : Entity
 {
-    public MainHero()
+    public MainHero(string name, Gender gender)
     {
         HP = HP == 0 ? 100 : HP;
+        Name = name;
+        Gender = gender;
     }
 }
