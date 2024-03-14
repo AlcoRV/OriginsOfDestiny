6b127b68f34b3d55f70d75055ac35fc79436321c
Коршунов <alexander.korshunov@advalange.com>
2024-03-14 21:32:17 +0300

    added extentions

---
+++ OriginsOfDestiny.Game/Extentions/GameExtentions.cs
@@ -0,0 +1,10 @@
+﻿using OriginsOfDestiny.Common.Interfaces.Storages;
+using OriginsOfDestiny.Data.Models.Entity;
+using OriginsOfDestiny.Game.Models.Actions;
+
+namespace OriginsOfDestiny.Game.Extentions;
+
+public static class GameExtentions
+{
+    public static HeroActions GetActions(this Hero hero, IGameData gameData) => new HeroActions(gameData, hero);
+}
--- OriginsOfDestiny.Game/Models/Actions/HeroActions.cs
+++ OriginsOfDestiny.Game/Models/Actions/HeroActions.cs
@@ -1,19 +1,14 @@
-﻿using OriginsOfDestiny.Common.Models.Storage;
+﻿using OriginsOfDestiny.Common.Interfaces.Storages;
 using OriginsOfDestiny.Data.Models.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OriginsOfDestiny.Game.Models.Actions
 {
     public class HeroActions
     {
-        private readonly GameData _gameData;
+        private readonly IGameData _gameData;
         private readonly Hero _hero;
 
-        public HeroActions(GameData gameData, Hero hero)
+        public HeroActions(IGameData gameData, Hero hero)
         {
             _gameData = gameData;
             _hero = hero;
