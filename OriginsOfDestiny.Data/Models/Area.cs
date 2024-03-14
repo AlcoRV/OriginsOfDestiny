6cfd5640dc85bd001ef8a9d25a956e2bc83ada0d
Коршунов <alexander.korshunov@advalange.com>
2024-03-14 21:31:59 +0300

    added locations

---
+++ OriginsOfDestiny.Data/Models/Area.cs
@@ -0,0 +1,8 @@
+﻿namespace OriginsOfDestiny.Data.Models;
+
+public class Area
+{
+    public string Name { get; set; }
+    public Location Location { get; set; }
+    public string Description { get; set; }
+}
--- OriginsOfDestiny.Data/Models/Location.cs
+++ OriginsOfDestiny.Data/Models/Location.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace OriginsOfDestiny.Data.Models;
 
-namespace OriginsOfDestiny.Data.Models
+public class Location
 {
-    public class Location
-    {
-    }
+    public string Name { get; set; }
+    public Area CentralArea { get; set; }
 }
